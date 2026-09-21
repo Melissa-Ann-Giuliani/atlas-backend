4a2639e74852cdbc9a0a7324b4c533c95a93c565
Melissa-Ann-Giuliani <melissa.a.giuliani@gmail.com>
2026-09-21 16:27:06 -0300

    DEV-36 RF-29 Arreglos
    -Arreglos a columnas de Auditor y AdminGlobal (asegurando que no admitan null y corrección de tamaño de acuerdo a la base de datos)
    -Actualización de Mockito para las pruebas

--- src/main/java/com/atlas/atlas_backend/usuario/Auditor.java
+++ src/main/java/com/atlas/atlas_backend/usuario/Auditor.java
@@ -12,6 +12,6 @@ import lombok.Setter;
 public class Auditor extends Usuario {
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "cargo_auditor_nombre", length = 50)
+    @Column(name = "cargo_aud_nombre", length = 40, nullable = false)
     private CargoAuditor cargoNombre;
 }
